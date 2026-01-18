98e1e4ae73b1d9bfafea1d21d1ad712395b19b32
IconicFish12 <superglidingogre0571@gmail.com>
2026-01-18 23:36:40 +0700

    [ADD]: Adding Crud Operation in User Module

--- src/module/user-module/user.module.ts
+++ src/module/user-module/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service.js';
 import { UserController } from './user.controller.js';
+import { DatabaseModule } from '../../common/database/database.module.js';
+import { DatabaseService } from '../../common/database/database.service.js';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, DatabaseService],
 })
 export class UserModule {}
